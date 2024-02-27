63b6bed102e7da5449d1a6bc113bdca3271f7a7e
xrasps <evelina.gazetdinova2003@gmail.com>
2024-02-27 19:20:09 +0300

    sm

---
+++ r7-r11/src/data.js
@@ -0,0 +1,38 @@
+export const catalog = [
+    {
+        id:1,
+        name:'Товар 1',
+        price: '500',
+        count: 0
+    },
+    {
+        id:2,
+        name:'Товар 2',
+        price: '500',
+        count: 0
+    },
+    {
+        id:3,
+        name:'Товар 3',
+        price: '500',
+        count: 4
+    },
+    {
+        id:4,
+        name:'Товар 4',
+        price: '500',
+        count: 2
+    },
+    {
+        id:5,
+        name:'Товар 5',
+        price: '500',
+        count: 0
+    },
+    {
+        id:6,
+        name:'Товар 6',
+        price: '500',
+        count: 0
+    },
+]
